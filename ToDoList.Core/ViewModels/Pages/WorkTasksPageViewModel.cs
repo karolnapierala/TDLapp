55cc6f7d4a38d446d06961f8a4e8822d97b3fa82
Karol Napierala <karoln109@gmail.com>
2022-01-10 21:10:32 +0100

    ToDoListApp

---
+++ ToDoList.Core/ViewModels/Pages/WorkTasksPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ToDoList.Core
+{
+    public class WorkTasksPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<WorkTaskViewModel> WorkTaskList { get; set; } = new ObservableCollection<WorkTaskViewModel>();
+
+        public string NewWorkTaskTitle { get; set; }
+        public string NewWorkTaskDescription { get; set; }
+        public ICommand AddNewTaskCommand { get; set; }
+        public ICommand DeleteSelectedTasksCommand { get; set; }
+        
+        public WorkTasksPageViewModel()
+        {
+            AddNewTaskCommand = new RelayCommand(AddNewTask);
+            DeleteSelectedTasksCommand = new RelayCommand(DeleteSelectedTasks);
+
+        }
+        private void AddNewTask()
+        {
+            var newTask = new WorkTaskViewModel
+            {
+                Title = NewWorkTaskTitle,
+                Description = NewWorkTaskDescription,
+                CreatedDate = DateTime.Now
+
+            };
+
+            WorkTaskList.Add(newTask);
+
+            NewWorkTaskTitle = string.Empty;
+            NewWorkTaskDescription = string.Empty;
+
+            OnPropertyChanged(nameof(NewWorkTaskTitle));
+            OnPropertyChanged(nameof(NewWorkTaskDescription));
+        }
+        private void DeleteSelectedTasks()
+        {
+            var selectedTasks = WorkTaskList.Where(x => x.IsSelected).ToList();
+            foreach (var task in selectedTasks)
+            {
+                WorkTaskList.Remove(task);
+            }
+        }
+    }
+}
